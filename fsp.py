30922679101fc579050da8c466fd71b0a1dcccbd
Wirgiliusz <kamx98@gmail.com>
2020-05-01 22:16:15 +0200

    Dodanie Cmax i Brute force [dziala]

--- fsp.py
+++ fsp.py
@@ -1,3 +1,8 @@
+import copy
+from itertools import permutations
+import math
+import timeit
+
 
 def loadData(path):
     plik = open(path, "r")
@@ -17,14 +22,50 @@ def loadData(path):
 
     return zadania
 
-
 def getOrder(zad):
     kolejnosc = []
     for i in range(0, len(zad)):
         kolejnosc.append(zad[i][-1])
     return kolejnosc
 
+def calculate_Cmax(zad):
+    S = []
+    C = []
+    Szad = []
+    Czad = []
+
+    Szad.append(0)
+    Czad.append(zad[0][0])
+
+    for i in range(0, len(zad)):
+        for j in range(0, len(zad[i])-1):
+            if i == 0 and j != 0:
+                Szad.append(Czad[j-1])
+                Czad.append(Szad[j] + zad[i][j])
+            elif i != 0:
+                if j == 0:
+                    Szad.append(C[i-1][0])
+                    Czad.append(Szad[0] + zad[i][0])
+                else:
+                    Szad.append(max(Czad[j-1], C[i-1][j]))
+                    Czad.append(Szad[j] + zad[i][j])
+
+        S.append(Szad.copy())
+        C.append(Czad.copy())
+        Szad.clear()
+        Czad.clear()
+
+    return C[-1][-1]
+
+def bruteForce(zad):
+    perms = list(permutations(zad,r=len(zad)))
+    Cmin = math.inf
+    for perm in perms:
+        C = calculate_Cmax(perm)
+        if C < Cmin:
+            Cmin = C
+    return Cmin
 
 zadania = loadData("data/data004.txt")
 
-print(zadania)
+print(bruteForce(copy.deepcopy(zadania)))
