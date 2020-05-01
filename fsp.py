924d2b8c66c00ab21cfdd1ea628c3ccb89e69d8c
Wirgiliusz <kamx98@gmail.com>
2020-05-01 20:59:50 +0200

    Dodanie wczytywania

---
+++ fsp.py
@@ -0,0 +1,30 @@
+
+def loadData(path):
+    plik = open(path, "r")
+    linie = plik.readlines()
+    n = int(linie[0].split()[0])
+    m = int(linie[0].split()[1])
+
+    zadania = []
+    for i in range(1, n+1):
+        linia = linie[i].split()
+        maszyny = []
+        for j in range(1, 2*m, 2):
+            maszyny.append(int(linia[j]))
+        maszyny.append(i)
+        zadania.append(maszyny)
+    plik.close()
+
+    return zadania
+
+
+def getOrder(zad):
+    kolejnosc = []
+    for i in range(0, len(zad)):
+        kolejnosc.append(zad[i][3])
+    return kolejnosc
+
+
+zadania = loadData("data/data004.txt")
+
+print(zadania)
